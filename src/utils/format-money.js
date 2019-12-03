7b151900e6daa82902c996b9c3efa84a09ec2f5e
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2019-12-03 14:54:36 -0800

    :bug: Fix a bug in rendering the movie property table

--- src/utils/format-money.js
+++ src/utils/format-money.js
@@ -1,3 +1,4 @@
-export default function formatMoney(v) {
-  return `$${v.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
+export default function formatMoney(val = 0) {
+  const v = val.toFixed(2);
+  return !v ? `$${v}` : `$${v.replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
 }
--- src/views/details/properties.js
+++ src/views/details/properties.js
@@ -4,11 +4,14 @@ function Properties({ data = {}, fields = [] }) {
   return (
     <div className="movie-details--properties columns is-gapless is-multiline">
       {fields.map(([key, label, transform]) => (
-        (key in data && data[key] && (
+        (key in data && (
           <React.Fragment key={key}>
             <div className="column is-5 is-size-7 has-text-weight-bold">{label}</div>
             <div className="column is-7">
-              {transform ? transform(data[key]) : data[key]}
+              {transform
+                ? transform(data[key])
+                : data[key]
+              }
             </div>
           </React.Fragment>
         ))
